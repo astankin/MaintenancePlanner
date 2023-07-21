bc6fa11a1887fa6354cc2dd8171179984b9d4c51
Atanas Stankin <a_stankin@abv.bg>
2023-07-21 09:46:48 +0300

    Updated 404 page

--- MaintenancePlanner/settings.py
+++ MaintenancePlanner/settings.py
@@ -18,6 +18,24 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "maintenance_planner_db",
+#         "USER": "postgres",
+#         "PASSWORD": "root",
+#         "HOST": "127.0.0.1",
+#         "PORT": "5433",
+#     }
+# }
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -80,23 +98,6 @@ WSGI_APPLICATION = 'MaintenancePlanner.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "maintenance_planner_db",
-#         "USER": "postgres",
-#         "PASSWORD": "root",
-#         "HOST": "127.0.0.1",
-#         "PORT": "5433",
-#     }
-# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
